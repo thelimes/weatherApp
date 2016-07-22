957afc4a042743fc552a6315d1c4a4db63a19b85
tiko9 <tikocharkviani@gmail.com>
2016-07-22 17:40:01 +0400

    sdfgdsg

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,14 +7,10 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+var cityName = "Tbilisi"
+class ViewController: UIViewController, UITextFieldDelegate {
     
-    
-    @IBOutlet weak var locationSearchBar: UISearchBar!
-    
-    
+    @IBOutlet weak var SearchFielf: UITextField!
     @IBOutlet weak var currentLocation: UILabel!
     @IBOutlet weak var currentDate: UILabel!
     @IBOutlet weak var currentWeekDay: UILabel!
@@ -38,8 +34,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.SearchFielf.delegate = self
+        parsingJson()
+
+    
+    }
+    
+    func parsingJson(){
+        currentLocation.text = cityName
         
-        let cityName = "Tbilisi"
         let url = "http://api.openweathermap.org/data/2.5/weather?q=\(cityName),uk&appid=2de143494c0b295cca9337e1e96b00e0"
         let URL = NSURL(string: url)!
         let session = NSURLSession.sharedSession()
@@ -50,10 +54,6 @@ class ViewController: UIViewController {
             if let responsedata = data {
                 do {
                     let json = try NSJSONSerialization.JSONObjectWithData(responsedata, options: NSJSONReadingOptions.AllowFragments)
-//                    print (json)
-//                    let array:NSArray = json["weather"] as! NSArray
-//                    let firstData:NSDictionary = array[0] as! NSDictionary
-//                    
                     
                     if let time = json["dt"] as? Int,
                         let h = json["main"]!!["humidity"] as? Int,
@@ -63,47 +63,49 @@ class ViewController: UIViewController {
                         let sunset = json["sys"]!!["sunset"] as? Int,
                         let w = json["wind"]!!["speed"] as? Double,
                         let d = json["wind"]!!["deg"] as? Int,
-                    let mainDescr = ((json["weather"]! as! NSArray)[0] as! NSDictionary)["description"] {
+                        let mainDescr = ((json["weather"]! as! NSArray)[0] as! NSDictionary)["description"] {
                             let myWeatherObject: WeatheObject = WeatheObject(time: time, humidity: h, pressure: p, temperature: t, sunrise: sunrise, sunset: sunset, maindescription: mainDescr as! String, windSpeed: w, degree: d)
-                        
-                        let datepicker = NSTimeInterval(time + 120)
-                        let date: NSDate = NSDate(timeIntervalSince1970: datepicker)
-                        let formatter = NSDateFormatter()
-                        formatter.dateStyle = NSDateFormatterStyle.MediumStyle
-                        formatter.timeStyle = .ShortStyle
-                        print(datepicker)
- 
-                        
-                        let timeOfSunRise = NSTimeInterval(sunrise + 120)
-                        let sunriseTime = NSDate(timeIntervalSince1970: timeOfSunRise)
-                        let calendar = NSCalendar.currentCalendar()
-                        let comp = calendar.components([.Hour, .Minute], fromDate: sunriseTime)
-                        let hourOfSunRise = comp.hour
-                        let minuteOfSunRise = comp.minute
-                        
-                        
-                        let timeOfSunset = NSTimeInterval(sunset + 120)
-                        let sunsetTime: NSDate = NSDate(timeIntervalSince1970: timeOfSunset)
-                        let compset = calendar.components([.Hour, .Minute], fromDate: sunsetTime)
-                        let hourOfSunset = compset.hour
-                        let minuteOfSunset = compset.minute
-                        
-                        
-                        let datestring: String = formatter.stringFromDate(date)
+                            
+                            let datepicker = NSTimeInterval(time + 120)
+                            let date: NSDate = NSDate(timeIntervalSince1970: datepicker)
+                            let formatter = NSDateFormatter()
+                            formatter.dateStyle = NSDateFormatterStyle.MediumStyle
+                            formatter.timeStyle = .ShortStyle
+                            print(datepicker)
+                            
+                            
+                            let timeOfSunRise = NSTimeInterval(sunrise + 180)
+                            let sunriseTime = NSDate(timeIntervalSince1970: timeOfSunRise)
+                            let calendar = NSCalendar.currentCalendar()
+                            let comp = calendar.components([.Hour, .Minute], fromDate: sunriseTime)
+                            let hourOfSunRise = comp.hour
+                            let minuteOfSunRise = comp.minute
+                            
+                            
+                            let timeOfSunset = NSTimeInterval(sunset + 180)
+                            let sunsetTime: NSDate = NSDate(timeIntervalSince1970: timeOfSunset)
+                            let compset = calendar.components([.Hour, .Minute], fromDate: sunsetTime)
+                            let hourOfSunset = compset.hour
+                            let minuteOfSunset = compset.minute
+                            
+                            
+                            let datestring: String = formatter.stringFromDate(date)
                             self.currentDate.text = "\(datestring)"
-                        
+                            
                             self.humidityLb.text = "\(myWeatherObject.humidity)%"
-
+                            
                             self.barometerLb.text = "\(myWeatherObject.pressure)hPa"
-
-                            self.weatherMainDescription.text = myWeatherObject.mainDescription 
-
+                            
+                            self.weatherMainDescription.text = myWeatherObject.mainDescription
+                            
                             self.sunriseTimeLb.text = "\(hourOfSunRise):\(minuteOfSunRise)am"
-
-                        self.sunsetTimeLb.text = "\(hourOfSunset):\(minuteOfSunset)pm"
+                            
+                            self.sunsetTimeLb.text = "\(hourOfSunset):\(minuteOfSunset)pm"
                             self.temperature.text = "\(myWeatherObject.temperature - 273)\u{00B0}"
                             self.windSpeedLb.text = "\(myWeatherObject.windSpeed)mph"
-
+                            print("fbdgdf")
+                            print("\(mainDescr)")
+                            
                     }
                     
                     print (json)
@@ -112,11 +114,8 @@ class ViewController: UIViewController {
                 }
             }
             print(error.debugDescription)
-        }.resume()
-
-    
+            }.resume()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -125,5 +124,19 @@ class ViewController: UIViewController {
         return UIStatusBarStyle.LightContent
     }
 
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        if SearchFielf.text != nil && SearchFielf.text != "" {
+            currentLocation.text = SearchFielf.text
+            cityName = SearchFielf.text!.capitalizedString
+            parsingJson()
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
 }
 
